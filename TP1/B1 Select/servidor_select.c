472a10857b3c8bcea588370278df36b72eca2485
zongadev <gonzimoyano@gmail.com>
2026-04-15 13:47:44 -0300

    Added TP2 files

---
+++ TP1/B1 Select/servidor_select.c
@@ -0,0 +1,123 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/select.h>
+
+#define PORT 12345
+#define MAX_CLIENTS 10
+#define BUFFER_SIZE 1024
+
+int main() {
+    int server_fd, new_socket, client_sockets[MAX_CLIENTS];
+    struct sockaddr_in address;
+    fd_set read_fds;
+    int max_sd, sd, activity, valread;
+    char buffer[BUFFER_SIZE];
+    
+    // Inicializar todos los clientes a 0
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        client_sockets[i] = 0;
+    }
+
+    // Crear socket
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket falló");
+        exit(EXIT_FAILURE);
+    }
+
+    // Configurar dirección del servidor
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Enlazar socket al puerto
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind falló");
+        exit(EXIT_FAILURE);
+    }
+
+    // Escuchar conexiones entrantes
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Servidor escuchando en puerto %d...\n", PORT);
+
+    while (1) {
+        // Limpiar conjunto de descriptores
+        FD_ZERO(&read_fds);
+        FD_SET(server_fd, &read_fds);
+        max_sd = server_fd;
+
+        // Agregar sockets de clientes al conjunto
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            sd = client_sockets[i];
+            if (sd > 0) FD_SET(sd, &read_fds);
+            if (sd > max_sd) max_sd = sd;
+        }
+
+        // Esperar actividad
+        activity = select(max_sd + 1, &read_fds, NULL, NULL, NULL);
+
+        if ((activity < 0) && (errno != EINTR)) {
+            printf("Error en select\n");
+        }
+
+        // Nueva conexión
+        if (FD_ISSET(server_fd, &read_fds)) {
+            socklen_t addrleen = sizeof(address);
+            if ((new_socket = accept(server_fd, (struct sockaddr *)&address, &addrleen)) < 0) {
+                perror("accept");
+                exit(EXIT_FAILURE);
+            }
+
+            printf("Nueva conexión: socket fd %d, IP %s, puerto %d\n",
+                   new_socket, inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+
+            // Agregar a la lista de clientes
+            for (int i = 0; i < MAX_CLIENTS; i++) {
+                if (client_sockets[i] == 0) {
+                    client_sockets[i] = new_socket;
+                    break;
+                }
+            }
+        }
+
+        // Revisar mensajes de clientes
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            sd = client_sockets[i];
+
+            if (FD_ISSET(sd, &read_fds)) {
+                if ((valread = read(sd, buffer, BUFFER_SIZE)) == 0) {
+                    // Desconexión
+                    socklen_t addrlen = sizeof(address);
+                    getpeername(sd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+                    printf("Cliente desconectado: IP %s, puerto %d\n",
+                           inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+                    close(sd);
+                    client_sockets[i] = 0;
+                } else {
+                    buffer[valread] = 0;
+                    printf("Mensaje recibido: %s", buffer);
+
+                    // Reenviar mensaje a otros clientes
+                    for (int j = 0; j < MAX_CLIENTS; j++) {
+                        if (client_sockets[j] != 0 && client_sockets[j] != sd) {
+                            send(client_sockets[j], buffer, strlen(buffer), 0);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ TP2/raw_sockets_ping.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/time.h>
+
+#define PACKET_SIZE 4096
+#define DEST_IP "127.0.0.1"
+#define MAX_PINGS 5
+
+unsigned short checksum(void *b, int len) {
+    unsigned short *buf = b;
+    unsigned int sum = 0;
+    for (; len > 1; len -= 2)
+        sum += *buf++;
+    if (len == 1)
+        sum += *(unsigned char*)buf;
+    sum = (sum >> 16) + (sum & 0xFFFF);
+    sum += (sum >> 16);
+    return ~sum;
+}
+
+int main() {
+    char packet[PACKET_SIZE];
+    struct sockaddr_in dest;
+    int sockfd, one = 1;
+    struct timeval timeout = {1, 0};  // 1 segundo de timeout
+
+    sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+    if (sockfd < 0) {
+        perror("socket");
+        return 1;
+    }
+
+    // Permite incluir cabecera IP
+    setsockopt(sockfd, IPPROTO_IP, IP_HDRINCL, &one, sizeof(one));
+    // Configura timeout de recepción
+    setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
+
+    dest.sin_family = AF_INET;
+    dest.sin_addr.s_addr = inet_addr(DEST_IP);
+
+    for (int seq = 1; seq <= MAX_PINGS; seq++) {
+        memset(packet, 0, PACKET_SIZE);
+
+        struct iphdr *ip = (struct iphdr *) packet;
+        struct icmphdr *icmp = (struct icmphdr *) (packet + sizeof(struct iphdr));
+
+        // IP Header
+        ip->ihl = 5;
+        ip->version = 4;
+        ip->tos = 0;
+        ip->tot_len = htons(sizeof(struct iphdr) + sizeof(struct icmphdr));
+        ip->id = htons(1234 + seq);
+        ip->frag_off = 0;
+        ip->ttl = 64;
+        ip->protocol = IPPROTO_ICMP;
+        ip->saddr = inet_addr("127.0.0.1");  // IP local para pruebas
+        ip->daddr = dest.sin_addr.s_addr;
+        ip->check = checksum((unsigned short *)ip, sizeof(struct iphdr));
+
+        // ICMP Header
+        icmp->type = ICMP_ECHO;
+        icmp->code = 0;
+        icmp->un.echo.id = htons(1234);
+        icmp->un.echo.sequence = htons(seq);
+        icmp->checksum = checksum((unsigned short *)icmp, sizeof(struct icmphdr));
+
+        if (sendto(sockfd, packet, ntohs(ip->tot_len), 0,
+                   (struct sockaddr *)&dest, sizeof(dest)) < 0) {
+            perror("sendto");
+        } else {
+            printf("Ping #%d enviado a %s...\n", seq, DEST_IP);
+        }
+
+        // Recibir respuesta
+        char recv_buffer[PACKET_SIZE];
+        struct sockaddr_in recv_addr;
+        socklen_t addr_len = sizeof(recv_addr);
+
+        ssize_t bytes = recvfrom(sockfd, recv_buffer, sizeof(recv_buffer), 0,
+                                 (struct sockaddr *)&recv_addr, &addr_len);
+
+        if (bytes < 0) {
+            if (errno == EAGAIN || errno == EWOULDBLOCK)
+                printf("Timeout esperando respuesta ICMP #%d\n", seq);
+            else
+                perror("recvfrom");
+        } else {
+            struct iphdr *recv_ip = (struct iphdr *)recv_buffer;
+            struct icmphdr *recv_icmp = (struct icmphdr *)(recv_buffer + (recv_ip->ihl * 4));
+
+            if (recv_icmp->type == ICMP_ECHOREPLY) {
+                printf("Respuesta ICMP #%d recibida desde %s\n\n",
+                       ntohs(recv_icmp->un.echo.sequence),
+                       inet_ntoa(recv_addr.sin_addr));
+            } else {
+                printf("Recibido otro tipo de paquete ICMP: tipo %d\n", recv_icmp->type);
+            }
+        }
+
+        sleep(1);  // esperar 1 segundo antes del próximo ping
+    }
+
+    close(sockfd);
+    return 0;
+}
